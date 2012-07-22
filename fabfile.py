229b5b0482cd6d2444ec2d949e6170f506c434bb
JAC <lennykey@gmx.de>
2012-07-22 19:12:23 +0200

    Cleaning up code and translations

--- fabfile.py
+++ fabfile.py
@@ -1,24 +1,42 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 
-from fabric.api import local, task, run
+from fabric.api import local, task, run, settings
 from fabric.tasks import Task
 from fabric.decorators import hosts
 
 
 @task
 def diskUsage():
-    local('df -h -T -x devtmpfs -x tmpfs')
+    '''Shows information about your systems space'''
+    with settings(warn_only=True):
+        capture = local('df afsdf -h -T -x devtmpfs -x tmpfs', capture=True)
+
+        if capture.failed:
+            print("Sorry :-/")
+        else:
+            print('*** This information were found ***')
+            print(capture)
 
 
-@task
-def update():
+def updateSystem():
     local('sudo apt-get update')
+
+
+def upgradeSystem():
     local('sudo apt-get upgrade')
 
 
 @task
-@hosts('localhost')
-def os_version():
-    #myvar = local('cat /etc/issue', capture=True)
+def update():
+    '''This task updates the sources list and upgrades the packages'''
+    print('*** Updating Package list ***')
+    updateSystem()
+    print('*** Upgrading System ***')
+    upgradeSystem()
+
+
+@task
+def osVersion():
+    '''Shows the version of your operating system'''
     myvar = local('cat /etc/issue', capture=False)
